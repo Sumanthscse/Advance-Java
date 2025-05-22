8dcc8db0d22d4e86e26387f397e80a906cc1ee64
Sumanthscse <sumanth27452@gmail.com>
2025-05-23 01:02:31 +0530

    Update ColorSearch.java

--- 1b.ArrayListPrograms/ColorSearch.java
+++ 1b.ArrayListPrograms/ColorSearch.java
@@ -1,3 +1,7 @@
+/* 1. Write a java program for getting different colors through ArrayList interface and search whether
+the color "Red" is available or not*/
+
+
 package prg1b;
 
 import java.util.ArrayList;
