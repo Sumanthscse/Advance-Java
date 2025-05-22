76f09cfb77cfac8e0f46b7bdc7f9220e570be9ae
Sumanthscse <sumanth27452@gmail.com>
2025-05-23 01:07:44 +0530

    Update RemoveNthColor.java

--- 1b.ArrayListPrograms/RemoveNthColor.java
+++ 1b.ArrayListPrograms/RemoveNthColor.java
@@ -1,3 +1,8 @@
+/* 6. Write a java program for getting different colors through ArrayList interface and delete nth
+element from the ArrayList object by using remove by index */
+
+
+
 package prg1b;
 
 import java.util.ArrayList;
